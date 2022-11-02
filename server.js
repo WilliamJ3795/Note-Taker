9513d6568e274b2ff888f5f13c3c7fe4bbadf86c
William James <Williamj3795@gmail.com>
2022-11-01 21:45:10 -0400

    Middleware and environment variable PORT

--- server.js
+++ server.js
@@ -8,3 +8,12 @@ const path = require("path");
 const fs = require("fs");
 //Uses callback funtions (req, res) and renders html elements based on passing arguments. 
 const app = express();
+
+
+const PORT = process.env.PORT || 3000;
+var dataNotes = [];
+
+// Middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
